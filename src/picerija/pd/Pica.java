03e6e8ae903b870aa08108b3aab733f144f366f5
Zorgenberga <annija.zorgenberga@gmail.com>
2022-01-26 22:50:04 +0200

    Izstradats picas pamats

--- src/picerija/pd/Pica.java
+++ src/picerija/pd/Pica.java
@@ -1,5 +1,20 @@
 package picerija.pd;
 
 public class Pica {
-
+	//Atributi
+	public int izmers;
+	public int PNr;
+	public boolean merce;
+	public boolean siers;
+	public boolean skinkis;
+	public boolean vista;
+	public boolean desa;
+	public boolean senes;
+	public boolean ananas;
+	public boolean pipari;
+	public boolean sipoli;
+	public boolean tomati;
+	public boolean lasis;
+	public boolean gurki;
+	
 }
