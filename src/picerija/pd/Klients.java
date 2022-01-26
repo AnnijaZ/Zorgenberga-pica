9d9b9c6437c3c0a9b762d14414e240f76556153a
Zorgenberga <annija.zorgenberga@gmail.com>
2022-01-26 23:53:52 +0200

    Izstradata klienta pamats

--- src/picerija/pd/Klients.java
+++ src/picerija/pd/Klients.java
@@ -1,5 +1,23 @@
 package picerija.pd;
 
 public class Klients {
-
+	//Atributi
+	String adrese;
+	String talrunis;
+	String vards;
+	int PasNr;
+	
+	//Konstruktors
+	public Klients(String KAdrese, String KTalrunis, String KVards, int KPasNr) {
+		this.adrese = KAdrese;
+		this.talrunis = KTalrunis;
+		this.vards = KVards;
+		this.PasNr = KPasNr;
+	}
+	
+	//Metodes
+	public int noteiktPasNr() {
+		return PasNr;
+	}
+	
 }
